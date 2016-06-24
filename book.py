9e52385353371ad2f35adc330f90552781650513
mwyrozebski <mwyrozebski@meil.pw.edu.pl>
2016-06-24 11:38:56 +0200

    python -- reading json

--- book.py
+++ book.py
@@ -1,7 +1,21 @@
-import json
 
-fdata = open('sample.json').read()
-
-data = json.loads( fdata )
-
-print(data)
+def readabstracts(fname):
+    import codecs
+    import json
+    
+    fdata = codecs.open(fname,encoding='utf8').read()
+    
+    data = json.loads( fdata )
+    
+    return data
+    
+    
+if __name__ == '__main__':
+    print('###############################')
+    print('# 8th European Postgraduate   #')
+    print('# Fluid Dynamics Conference   #')
+    print('# Book of Abstracts generator #')
+    print('###############################')
+    
+    data = readabstracts('abstracts.json')
+    
